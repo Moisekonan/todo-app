88865160f4e399e59370db8462166114180ba067
Moisekonan <kouassimoise.konan@epinesdorsales.com>
2024-01-26 17:06:44 +0000

    fini le CRUD

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { Todo } from './interfaces/todo.interface';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 // localhost:3000/todos
 @Controller('todos')
@@ -21,5 +22,15 @@ export class TodosController {
     createTodo(@Body() newTodo: Todo) {
         console.log('new', newTodo);
         this.todosService.create(newTodo);
+    }
+
+    @Patch(':id')
+    updateTodo(@Param('id') id: string, @Body() todo: CreateTodoDto) {
+        return this.todosService.update(id, todo);
+    }
+
+    @Delete(':id')
+    deleteTodo(@Param('id') id: string) {
+        return this.todosService.delete(id);
     }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './interfaces/todo.interface';
 import { CreateTodoDto } from './dto/create-todo.dto';
 
@@ -35,5 +35,45 @@ export class TodosService {
 
   create(todo: CreateTodoDto) {
     this.todos = [...this.todos, todo as Todo];
+  }
+
+  update(id: string, todo: Todo) {
+    const todoToUpdate = this.todos.find(todo => todo.id === +id);
+    if (!todoToUpdate) {
+      return new NotFoundException('booo did you fing his todo');
+    }
+
+    if (todo.hasOwnProperty('done')) {
+      todoToUpdate.done = todo.done;
+    }
+    if (todo.title) {
+      todoToUpdate.title = todo.title;
+    }
+    if (todo.description) {
+      todoToUpdate.description = todo.description;
+    }
+
+    const updatedTodos = this.todos.map(t => t.id !== +id ? t : todoToUpdate);
+    this.todos = [...updatedTodos];
+    return {
+      updatedTodo: 1,
+      todo: todoToUpdate,
+    }
+  }
+
+  delete(id: string) {
+    const nbOfTodosBeforeDelete = this.todos.length;
+    this.todos = [...this.todos.filter(t => t.id !== +id)];
+    if (this.todos.length < nbOfTodosBeforeDelete) {
+      return {
+        deletedTodo: 1,
+        nbTodos: this.todos.length,
+      };
+    } else {
+      return {
+        deletedTodo: 0,
+        nbTodos: this.todos.length,
+      };
+    }
   }
 }
